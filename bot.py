00b20d0985ff900e0228b0bec66c77e6870c8fa2
amiri <aminamiri144@gmail.com>
2025-12-27 00:22:08 +0330

    add sponsered

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8550969476:AAFOqTCzfYuVLJlypzAu52K_W_1ygzF-y
 # تعریف تصاویر دعوت‌نامه
 # فرمت: {'key': {'name': 'نام نمایشی', 'image': 'مسیر فایل تصویر', 'name_position': (x, y), 'signature_position': (x, y)}}
 
-IMAGE = 'main_poster.jpg'
+IMAGE = 'main_poster.JPG'
 name_position = (970, 1200)
 signature_position = (970, 1400)
 
